8911cbc33b136153d39d1d76913e4fa6a3446bb9
David Chen <chen7david@me.com>
2019-12-26 18:39:30 +0800

    update

---
+++ src/components/ui/Navbar.vue
@@ -0,0 +1,119 @@
+<template>
+  <div>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.text"
+          link
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-subheader class="mt-4 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
+        <v-list>
+          <v-list-item
+            v-for="item in items2"
+            :key="item.text"
+            link
+          >
+            <v-list-item-avatar>
+              <img
+                :src="`https://randomuser.me/api/portraits/men/${item.picture}.jpg`"
+                alt=""
+              >
+            </v-list-item-avatar>
+            <v-list-item-title v-text="item.text" />
+          </v-list-item>
+        </v-list>
+        <v-list-item
+          class="mt-4"
+          link
+        >
+          <v-list-item-action>
+            <v-icon color="grey darken-1">mdi-plus-circle-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-title class="grey--text text--darken-1">Browse Channels</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon color="grey darken-1">mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-title class="grey--text text--darken-1">Manage Subscriptions</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer> 
+    <v-app-bar
+      app
+      clipped-left
+      dense
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>AOX</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn  text>Link 1</v-btn>
+        <v-btn text>Link 2</v-btn>
+        <v-btn text>Link 3</v-btn>
+      </v-toolbar-items>
+
+      <template v-if="$vuetify.breakpoint.smAndUp">
+        <v-btn icon>
+          <v-icon>mdi-export-variant</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-delete-circle</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-plus-circle</v-icon>
+        </v-btn>
+        <v-switch 
+         v-model="darkMode"
+         hide-details
+        ></v-switch>
+      </template>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Navbar',
+    data: () => ({
+        drawer:null,
+        darkMode: false,
+        items: [
+            { icon: 'mdi-account', text: 'Users' },
+            { icon: 'mdi-account', text: 'Subscriptions' },
+            { icon: 'mdi-account', text: 'History' },
+            { icon: 'mdi-account', text: 'Playlists' },
+            { icon: 'mdi-account', text: 'Watch Later' },
+        ],
+        items2: [
+            { picture: 28, text: 'Joseph' },
+            { picture: 38, text: 'Apple' },
+            { picture: 48, text: 'Xbox Ahoy' },
+            { picture: 58, text: 'Nokia' },
+            { picture: 78, text: 'MKBHD' },
+        ]
+    }),
+    watch: {
+        darkMode(){
+            this.$vuetify.theme.dark = this.darkMode 
+        }
+    },
+}
+</script>
