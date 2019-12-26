93b5ef88a7e5278b9eba38f77a302f9afa5a6140
David Chen <chen7david@me.com>
2019-12-26 19:28:56 +0800

    update

--- src/views/Students.vue
+++ src/views/Students.vue
@@ -1,10 +1,28 @@
 <template>
     <v-container>
         <apollo-query :query="require('./../graphql/users/index.gql')">
-            <template v-slot="{ result: {loading, error, data}}">  
-                {{data}}
+            <template v-slot="{ result: {loading, error, data}}">
+                <div v-if="loading">loading ...</div>
+                <v-list three-one v-else-if="data" v-for="user in data.users" :key="user.userId">
+                    <v-list-item>
+                        <v-list-item-action>
+                            <v-icon x-large>mdi-account</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>{{user.username}}</v-list-item-title>
+                            <v-list-item-subtitle>{{user.userId}}</v-list-item-subtitle>
+                        </v-list-item-content>
+                        <v-list-item-action>
+                            <v-btn fab text><v-icon>mdi-pencil</v-icon></v-btn>
+                        </v-list-item-action>
+                        <v-list-item-action>
+                            <v-btn fab text><v-icon>mdi-delete</v-icon></v-btn>
+                        </v-list-item-action>
+                    </v-list-item>
+                </v-list>
             </template>
         </apollo-query>
+        
     </v-container>
 </template>
 
